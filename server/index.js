ab0fc637fd280c5cac3cb67e1405f29e6bb4516d
jinguchi <pohl9000@gmail.com>
2022-04-05 00:29:33 +0800

    restarting JavascriptMastery's tut

--- server/index.js
+++ server/index.js
@@ -2,3 +2,24 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+const app = express();
+
+// didnt need to use bodyParser on this one
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+// we will be using mongodb's cloud atlas version which means they will host our DB on their cloud
+const CONNECTION_URL = "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.9loo9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+// useNewUrlParser and useUnifiedTopology is just so we dont get errors
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`))
+    }).catch((error) => {
+        console.log(error.message)
+    })
+
+// below is just so we dont get errors
+// mongoose.set('useFindAndModify', false)
