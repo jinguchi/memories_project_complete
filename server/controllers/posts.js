13def854a2a680fe5eb48ba07b15c6aa75bee730
jinguchi <pohl9000@gmail.com>
2022-04-09 23:30:51 +0800

    29:15

---
+++ server/controllers/posts.js
@@ -0,0 +1,6 @@
+// this will contain all the logics/functions of the routes
+export const getPosts = (req, res) => {
+    res.send('bomb')
+}
+
+ 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import {getPosts} from '../controllers/posts.js' // we can omit .js in React but not in node
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('This works')
-})
+router.get('/', getPosts)
 
 export default router;
