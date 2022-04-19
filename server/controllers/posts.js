d72bef2d5530ca7754fc38224feb3fec3c149ea2
jinguchi <pohl9000@gmail.com>
2022-04-19 16:34:35 +0800

    halt to decide if upgrading react for @mui/core

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,29 @@
+import PostMessage from "../models/postMessage"
+
 // this will contain all the logics/functions of the routes
-export const getPosts = (req, res) => {
-    res.send('bomb')
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        // console.log(postMessages);
+
+        // learn more about status codes here: https://www.restapitutorial.com/httpstatuscodes.html
+        // the ones with star are the most important
+        res.status(200).json(postMessages)
+    } catch (error) {
+        req.status(404).json({message: error.message})
+    }
 }
 
- 
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+    try {
+        // res.send('Post Creation')
+        await newPost.save()
+        
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message: error.message})       
+    }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {getPosts} from '../controllers/posts.js' // we can omit .js in React but not in node
+import {getPosts, createPost} from '../controllers/posts.js' // we can omit .js in React but not in node
 
 const router = express.Router()
 
 router.get('/', getPosts)
+router.post('/', createPost)
 
 export default router;
